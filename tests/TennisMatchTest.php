6ff705b55eb3fe7107310d090c6745202311a786
tcheuD <tcheutcheud@gmail.com>
2020-10-01 12:31:03 +0200

    kata: TennisMatch TDD

---
+++ tests/TennisMatchTest.php
@@ -0,0 +1,49 @@
+<?php
+
+use App\Player;
+use App\TennisMatch;
+use PHPUnit\Framework\TestCase;
+
+class TennisMatchTest extends TestCase
+{
+    /**
+     * @test
+     * @dataProvider scores
+     */
+    public function it_scores_a_tennis_match($playerOnePoints, $playerTwoPoints, $score): void
+    {
+        $match = new TennisMatch(
+            $john = new Player('John'),
+            $jane = new Player('Jane'),
+        );
+
+        for ($i = 0; $i < $playerOnePoints; $i++) {
+            $john->score();
+        }
+
+        for ($i = 0; $i < $playerTwoPoints; $i++) {
+            $jane->score();
+        }
+
+        self::assertEquals($score, $match->score());
+    }
+
+    public function scores(): array
+    {
+        return [
+            [0, 0, 'love-love'],
+            [1, 0, 'fifteen-love'],
+            [1, 1, 'fifteen-fifteen'],
+            [2, 0, 'thirty-love'],
+            [3, 0, 'forty-love'],
+            [2, 2, 'thirty-thirty'],
+            [3, 3, 'deuce'],
+            [4, 4, 'deuce'],
+            [5, 5, 'deuce'],
+            [4, 3, 'Advantage: John'],
+            [3, 4, 'Advantage: Jane'],
+            [4, 0, 'Winner: John'],
+            [0, 4, 'Winner: Jane'],
+        ];
+    }
+}
